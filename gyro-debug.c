887dcc7e13093e76d9d5adba4c16d9a376926940
AJ Stubberud <stubberudaj@gmail.com>
2014-01-16 19:43:57 -0800

    Add gyro sensor debug program

---
+++ gyro-debug.c
@@ -0,0 +1,48 @@
+#pragma config(Sensor, S1,     gyro,           sensorI2CHiTechnicGyro)
+
+#define TIME_MAX 0xFFFF
+
+bool initialized = false;
+unsigned long lastUpdate = 0;
+
+long getTimeDelta() {
+	if (!initialized) {
+		initialized = true;
+		lastUpdate = nSysTime;
+		return 0;
+	}
+
+	long delta;
+
+	if (nSysTime < lastUpdate) {
+		// The time has looped around
+		delta = TIME_MAX - lastUpdate + nSysTime;
+		lastUpdate = nSysTime;
+	} else {
+		delta = nSysTime - lastUpdate;
+		lastUpdate = nSysTime;
+	}
+
+	return delta;
+}
+
+task main() {
+
+	int zero = 0;
+	float angle = 0;
+
+	// Find the zero
+	for (int i = 0; i < 10; i++) {
+		zero += SensorValue[gyro];
+		wait1Msec(100);
+	}
+	zero /= 10;
+
+	while(true) {
+		angle += ((SensorValue[gyro]) - zero) * getTimeDelta() / 1000.0;
+
+		nxtDisplayTextLine(0, "%d", SensorValue[gyro]);
+		nxtDisplayTextLine(1, "%.4f", angle);
+		nxtDisplayTextLine(2, "%d", zero);
+	}
+}
