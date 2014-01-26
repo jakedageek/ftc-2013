ae17b418f490c8bee63b8cf47770e6bbd27dcc5b
AJ Stubberud <stubberudaj@gmail.com>
2014-01-26 14:52:42 -0800

    Add turnRightT

--- lib/gyro.h
+++ lib/gyro.h
@@ -26,6 +26,7 @@
 
 void calibrateGyro();
 void turnRight(float degrees, int speed);
+void turnRightT(float degrees, int speed);
 void turnLeft(float degrees, int speed);
 void turnLeftT(float degrees, int speed);
 int getGyroValue();
@@ -58,6 +59,25 @@ void turnRight(float degrees, int speed) {
 	while(abs(turned) < degrees) {
 		float sValue = SensorValue[gyro];
 		float delta = getTimeDelta();
+		turned += (sValue - gyro_zero) * delta / 1000.0;
+	}
+
+	motor[leftDrive] = 0;
+	motor[rightDrive] = 0;
+}
+
+void turnRightT(float degrees, int speed) {
+	float turned = 0;
+
+	wait1Msec(1000);
+	resetTimeDeltaTimer();
+	getTimeDeltaTimer();
+	motor[leftDrive] = speed;
+	motor[rightDrive] = -speed;
+
+	while(abs(turned) < degrees) {
+		float sValue = SensorValue[gyro];
+		float delta = getTimeDeltaTimer();
 		turned += (sValue - gyro_zero) * delta / 1000.0;
 	}
 
