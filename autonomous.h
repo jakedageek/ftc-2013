1d30096693b7d89e2c61a24704ec83b3085311a0
AJ Stubberud <stubberudaj@gmail.com>
2014-03-18 21:01:13 -0700

    Add turnLeftEuler

--- autonomous.h
+++ autonomous.h
@@ -8,6 +8,7 @@ void driveBackward(int millis, int speed);
 void driveBackward(int speed);
 void driveStop();
 void turnRightEuler(int degrees, int speed);
+void turnLeftEuler(int degrees, int speed);
 int gyroValue();
 
 int gyro_zero;
@@ -54,6 +55,27 @@ void turnRightEuler(int degrees, int speed) {
 
 	motor[leftDrive] = speed;
 	motor[rightDrive] = -speed;
+
+	ClearTimer(T4);
+	lastTime = time1[T4];
+
+	while (abs(rotated) < degrees) {
+		int g_val = gyroValue();
+		int dt = time1[T4] - lastTime;
+		lastTime = time1[T4];
+
+		rotated += (float) dt / 1000.0 * (float) g_val;
+	}
+
+	driveStop();
+}
+
+void turnLeftEuler(int degrees, int speed) {
+	float rotated = 0;
+	int lastTime;
+
+	motor[leftDrive] = -speed;
+	motor[rightDrive] = speed;
 
 	ClearTimer(T4);
 	lastTime = time1[T4];
